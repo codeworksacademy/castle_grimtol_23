73531b5a404d2ec73de708e5f0436a04c0bfbca7
Andrew Rady <andrew.arsoftware@gmail.com>
2023-10-27 22:44:13 -0600

    Scaffold express setup

---
+++ index.ts
@@ -0,0 +1,15 @@
+import express, { Request, Response } from 'express';
+import path from 'path';
+
+const app = express();
+const port = process.env.PORT || 3000;
+app.use(express.static('assets'));
+
+app.get('/', (res: Response) => {
+    res.sendFile(path.join(__dirname, '/index.html'));
+});
+
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+});
