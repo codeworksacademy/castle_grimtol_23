f0e25a87765bfe3ff5a62a9ed8f22f0bc42ef47b
Andrew Rady <andrew.arsoftware@gmail.com>
2023-10-28 13:47:21 -0600

    call open ai for images

--- extensions/OpenAI.ts
+++ extensions/OpenAI.ts
@@ -3,17 +3,17 @@ import OpenAI from 'openai';
 export class OpenAi {
 
     private readonly openai = new OpenAI({
-        apiKey: ''
+        apiKey: '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'
     });
 
-    public async getImage(): Promise<string> {
-        const response = this.openai.createImage({
-            prompt: '',
-            n: 1,
+    public async getImage(prompt: string): Promise<Array<string | undefined>> {
+        const response = await this.openai.images.generate({
+            prompt: prompt,
+            n: 5,
             size: '1024x1024'
         });
 
-        const image = response.data.data[0].url;
+        const image = response.data.map(x => x.url);
 
         return image;
     }
--- index.ts
+++ index.ts
@@ -1,19 +1,17 @@
 import express, { Request, Response } from 'express';
-//import { OpenAi } from './extensions/OpenAI';
+import { OpenAi } from './extensions/OpenAI';
 
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(express.static('assets'));
 app.set('view engine', 'ejs');
-//const openid = new OpenAi();
+const openid = new OpenAi();
 
 app.get('/', async (req: Request, res: Response) => {
-    //const image:string = await openid.getImage();
-    const image = '123'
+    const images:Array<string | undefined> = await openid.getImage('vampire girl');
 
-    //res.sendFile(path.join(__dirname, '/index.html'));
     res.render('index', {
-        image: image
+        images: images
     });
 });
 
